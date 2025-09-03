6e24b4f687f2223d97c988b36278900fb21aff0f
yukta mahajan <yuktamahajan404@gmail.com>
2025-09-03 19:30:15 +0530

    Initial backend push

--- App_Backend/index.js
+++ App_Backend/index.js
@@ -15,7 +15,7 @@ app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 app.use("/schools", schoolRoutes);
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
